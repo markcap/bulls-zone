72fa0a8d9142990fbc8e158a61babc452063304f
Mark Capodagli <pimea.mark@gmail.com>
2011-02-18 15:53:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bulls::Application.config.secret_token = 'ada3b7c7950172232129755408d9f37ba979cd99b92738055daeba31eb011262008c069b6f5aada84b8c761a84f29a272536a06c543fd53791c71514922bb15c'
